51c972ad25cf1afa47373c8c2f49355f2f813ad4
Jeong-Hayoon <137061867+Jeong-Hayoon@users.noreply.github.com>
2025-09-03 17:41:19 +0900

    [Add] TicTacToe 블록, 게임 로직 구현

---
+++ Assets/02. Scripts/Game/GameUIController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class GameUIController : MonoBehaviour
+{
+    public void OnClickBackButton()
+    {
+        GameManager.Instance.OpenConfirmPanel("게임을 종료하시겠습니까?",
+            () =>
+            {
+                GameManager.Instance.ChangeToMainScene();
+            });
+    }
+}
--- Assets/02. Scripts/Popup/PanelController.cs
+++ Assets/02. Scripts/Popup/PanelController.cs
@@ -2,41 +2,48 @@ using System;
 using DG.Tweening;
 using UnityEngine;
 
-// 필수 컴포넌트
 [RequireComponent(typeof(CanvasGroup))]
 public class PanelController : MonoBehaviour
 {
     // 팝업창 RectTransform
     [SerializeField] private RectTransform panelRectTransform;
 
-    // 클래스 안에 있는 멤버 변수라서 언더바
     private CanvasGroup _backgroundCanvasGroup;
+
+    // Panel이 Hide 될 때 해야 할 동작
+    public delegate void PanelControllerHideDelegate();
 
     private void Awake()
     {
-        // GetComponent도 성능이 조금 드는 함수
         _backgroundCanvasGroup = GetComponent<CanvasGroup>();
     }
 
+    /// <summary>
     /// Panel 표시
+    /// </summary>
     public void Show()
     {
-        // 투명하고 사이즈를 0으로 초기화
         _backgroundCanvasGroup.alpha = 0;
         panelRectTransform.localScale = Vector3.zero;
 
-        // Dotween 함수 활용하여 점점 커지면서 
         _backgroundCanvasGroup.DOFade(1, 0.3f).SetEase(Ease.Linear);
         panelRectTransform.DOScale(1, 0.3f).SetEase(Ease.OutBack);
     }
 
+    /// <summary>
     /// Panel 숨기기
-    public void Hide()
+    /// </summary>
+    public void Hide(PanelControllerHideDelegate hideDelegate = null)
     {
         _backgroundCanvasGroup.alpha = 1;
         panelRectTransform.localScale = Vector3.one;
 
         _backgroundCanvasGroup.DOFade(0, 0.3f).SetEase(Ease.Linear);
-        panelRectTransform.DOScale(0, 0.3f).SetEase(Ease.InBack);
+        panelRectTransform.DOScale(0, 0.3f).SetEase(Ease.InBack)
+            .OnComplete(() =>
+            {
+                hideDelegate?.Invoke();
+                Destroy(gameObject);
+            });
     }
 }
